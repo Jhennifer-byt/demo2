ca9c6e818bd5f3958e98654d99c2262f1c48bfae
Jhennifer-byt <118638742+Jhennifer-byt@users.noreply.github.com>
2024-08-26 17:31:53 -0500

    cambios yml y base

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 	function conexion(){
 
-	$host = "host=viaduct.proxy.rlwy.net";
-	$port = "port=26862";
-	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "password=SIIZpYZuHrHzKtzfUhAFNdDKKPEACBSJ";
+	$host = "dpg-cr6g03bv2p9s7392u6s0-a";
+	$port = "port=5432";
+	$dbname = "dbname=dbprueba_hydx";
+	$user = "user=dbprueba_hydx_user";
+	$password = "password=sXiVp5XxNE8xMMKvqg7THkLpun3gD5v4";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
