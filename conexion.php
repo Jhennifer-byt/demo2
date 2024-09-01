49f7472d0e9df072585c1954ddc8a6d774785e18
Jhennifer-byt <118638742+Jhennifer-byt@users.noreply.github.com>
2024-09-01 02:28:53 -0500

    Cambio en la base de datos para postgres

--- conexion.php
+++ conexion.php
@@ -1,14 +1,22 @@
 <?php
-	function conexion(){
+function conexion() {
+    $host = "dpg-cr6g03bv2p9s7392u6s0-a";
+    $port = "5432"; // No es necesario poner "port=" dentro de la variable
+    $dbname = "dbprueba_hydx"; // Similarmente, sólo el nombre de la base de datos
+    $user = "dbprueba_hydx_user"; // Solo el usuario
+    $password = "sXiVp5XxNE8xMMKvqg7THkLpun3gD5v4"; // Solo la contraseña
 
-	$host = "dpg-cr6g03bv2p9s7392u6s0-a";
-	$port = "port=5432";
-	$dbname = "dbname=dbprueba_hydx";
-	$user = "user=dbprueba_hydx_user";
-	$password = "password=sXiVp5XxNE8xMMKvqg7THkLpun3gD5v4";
+    // Crear la cadena de conexión correcta
+    $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+    
+    $db = pg_connect($connection_string);
 
-	$db = pg_connect("$host $port $dbname $user $password");
+    if (!$db) {
+        echo "Error: No se pudo conectar a la base de datos.\n";
+    } else {
+        echo "Conexión exitosa.\n";
+    }
 
-	return $db;
+    return $db;
 }
 ?>
